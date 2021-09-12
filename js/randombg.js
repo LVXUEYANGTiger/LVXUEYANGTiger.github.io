3bb7b52a187cfb8a8691ae244a793373fbed49b3
LVXUEYANGTiger <1191432275@qq.com>
2021-09-12 12:19:47 +0800

    Site updated: 2021-09-12 12:19:08

--- js/randombg.js
+++ js/randombg.js
@@ -1,7 +1,10 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
+  "url(https://blog-1259153703.cos.ap-nanjing.myqcloud.com/images/20210912121348.jpg)",
   "url(https://lvxueyangboke.oss-cn-beijing.aliyuncs.com/images/20210830103321.jpg)",
+  "url(https://blog-1259153703.cos.ap-nanjing.myqcloud.com/images/20210912121348.jpg)",
   "url(https://lvxueyangboke.oss-cn-beijing.aliyuncs.com/images/20210830103450.jpg)",
+  "url(https://blog-1259153703.cos.ap-nanjing.myqcloud.com/images/20210912121348.jpg)",
   "url(https://lvxueyangboke.oss-cn-beijing.aliyuncs.com/images/20210906142902.jpg)",
   "url(https://lvxueyangboke.oss-cn-beijing.aliyuncs.com/images/20210830103321.jpg)"
 ];
