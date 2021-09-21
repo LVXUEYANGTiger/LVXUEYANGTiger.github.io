e42e177230e840940a6330722577f5caa578c9dc
LVXUEYANGTiger <1191432275@qq.com>
2021-09-21 11:05:28 +0800

    Site updated: 2021-09-21 11:04:50

--- js/randombg.js
+++ js/randombg.js
@@ -1,12 +1,9 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
   "url(https://blog-1259153703.cos.ap-nanjing.myqcloud.com/images/20210912121348.jpg)",
-  "url(https://lvxueyangboke.oss-cn-beijing.aliyuncs.com/images/20210830103321.jpg)",
   "url(https://blog-1259153703.cos.ap-nanjing.myqcloud.com/images/20210912121348.jpg)",
   "url(https://lvxueyangboke.oss-cn-beijing.aliyuncs.com/images/20210830103450.jpg)",
-  "url(https://blog-1259153703.cos.ap-nanjing.myqcloud.com/images/20210912121348.jpg)",
-  "url(https://lvxueyangboke.oss-cn-beijing.aliyuncs.com/images/20210906142902.jpg)",
-  "url(https://lvxueyangboke.oss-cn-beijing.aliyuncs.com/images/20210830103321.jpg)"
+  "url(https://blog-1259153703.cos.ap-nanjing.myqcloud.com/images/20210912121348.jpg)"
 ];
 //获取背景图片总数，生成随机数
 var bgindex =Math.ceil(Math.random() * (backimg.length-1));
